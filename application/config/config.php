5e1959ae4dc25dc4d9d7b05ed748dfac366b9d85
Danmer <danmerccoscco@gmail.com>
2019-01-26 23:14:17 -0500

    test de prueba

--- application/config/config.php
+++ application/config/config.php
@@ -26,6 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$config['base_url'] = 'http://inscripciones.caen.edu.pe';
 $config['base_url'] = 'http://inscripciones.test.com';
 /*
+|
 |--------------------------------------------------------------------------
 | Index File
 |--------------------------------------------------------------------------
