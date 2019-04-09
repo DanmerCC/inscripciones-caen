22e7c81c258ad904eec8f5106476bdb73a897d1a
Danmer Ccoscco <danmerccoscco@gmail.com>
2019-04-09 16:51:18 -0500

    Actualizacion Ultima modificacion modal 09-04

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -430,7 +430,7 @@ $.ajax({
                                 "</div>"+    
                             "</div>"+
                             "<div class='modal-footer'>"+
-                                "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Cerrar</button>"+
+                                "<button type='button' class='btn btn-primary' data-dismiss='modal'>Cerrar</button>"+
                             "</div>"+
                         "</div>"+
                     "</div>"+
