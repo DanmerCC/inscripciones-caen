fde2c388391b51513b0b90c5d823ea355f2944ae
Danmer Ccoscco <danmerccoscco@gmail.com>
2019-05-17 18:04:54 -0500

    api persona completo

--- application/config/routes.php
+++ application/config/routes.php
@@ -225,7 +225,8 @@ $route['api/inscrito/persona'] = 'rest/apipersona_Controller/persona/id/';
 //**Rest codigo propio  v 0.1*/
 $route['api/v1/inscritos'] = 'apiRest/Inscritos_Controller/get';
 $route['api/v1/inscrito/(:num)'] = 'apiRest/Inscritos_Controller/getById/$1';
-
+$route['api/v1/personas'] = 'apiRest/Persona_Controller/get';
+$route['api/v1/persona/(:num)'] = 'apiRest/Persona_Controller/getById/$1';
 
 /***
  * Chart estadistics
