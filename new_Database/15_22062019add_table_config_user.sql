3dcb02bca9133366d8fbc7383fbb5494bdc81079
Carlos Ysique Chavez <desarrollo.tic@caen.epg>
2019-06-24 09:10:23 -0500

    sql

--- new_Database/15_22062019add_table_config_user.sql
+++ new_Database/15_22062019add_table_config_user.sql
@@ -3,6 +3,12 @@ CREATE TABLE `config_perfil_user` (
   `acordion_default_name` varchar(25) NOT NULL,
   `usuario_id` int(10) NOT NULL
 )
+ALTER TABLE `config_perfil_user` ADD PRIMARY KEY(`id_config_perfil_user`);
 
 ALTER TABLE `config_perfil_user` ADD INDEX(`usuario_id`);
 ALTER TABLE `config_perfil_user` CHANGE `usuario_id` `usuario_id` INT(10) UNSIGNED NOT NULL;
+
+
+--
+ALTER TABLE `config_perfil_user`
+  ADD CONSTRAINT `config_perfil_user_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
