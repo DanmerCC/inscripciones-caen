1348c8637c28cbdd8a23a7019cde26e958d84a90
Danmer Ccoscco <danmerccoscco@gmail.com>
2019-10-01 16:50:45 -0500

    fechas

--- application/models/UseStadistics_model.php
+++ application/models/UseStadistics_model.php
@@ -11,7 +11,7 @@ class UseStadistics_model extends CI_Model
 	}
 
 	function count_inscripcions_day_month(){
-		$result=$this->db->query("SELECT  COUNT(id_inscripcion) as cantidad,MONTH(created)as mes,DAY(created) as dia FROM inscripcion GROUP BY DAY(created), MONTH(created)");
+		$result=$this->db->query("SELECT  COUNT(id_inscripcion) as cantidad,DATE(created) AS fecha  FROM inscripcion GROUP BY DAY(created), MONTH(created) , YEAR(created) ORDER BY created asc");
 		return $result->result_array();
 	}
 }
