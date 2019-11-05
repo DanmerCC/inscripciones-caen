1c4994350a9624810ede67a166ee3e72b77a7567
yony quispe ramos <jtyony18@gmail.com>
2019-11-05 14:50:46 -0500

    Creacion de folders para linux

---
+++ FolderCreator.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+if [ ! -d uploads ]; then
+    mkdir -p uploads;
+fi 
+
+if [ ! -d uploads/files ]; then
+    mkdir -p uploads/files;
+fi 
+if [ ! -d uploads/trash ]; then
+    mkdir -p uploads/trash;
+fi
+if [ ! -d uploads/files/bachiller ]; then
+    mkdir -p uploads/files/bachiller;
+fi 
+if [ ! -d uploads/files/cvs ]; then
+    mkdir -p uploads/files/cvs;
+fi 
+if [ ! -d uploads/files/djs ]; then
+    mkdir -p uploads/files/djs;
+fi 
+if [ ! -d uploads/files/dni ]; then
+    mkdir -p uploads/files/dni;
+fi 
+if [ ! -d uploads/files/doctorado ]; then
+    mkdir -p uploads/files/doctorado;
+fi 
+if [ ! -d uploads/files/foto ]; then
+    mkdir -p uploads/files/foto;
+fi 
+if [ ! -d uploads/files/hojadatos ]; then
+    mkdir -p uploads/files/hojadatos;
+fi 
+if [ ! -d uploads/files/maestria ]; then
+    mkdir -p uploads/files/maestria;
+fi 
+if [ ! -d uploads/files/proinves ]; then
+    mkdir -p uploads/files/proinves;
+fi 
+if [ ! -d uploads/files/sInscripcion ]; then
+    mkdir -p uploads/files/sInscripcion;
+fi 
+if [ ! -d uploads/files/sol-ad ]; then
+    mkdir -p uploads/files/sol-ad;
+fi 
+if [ ! -d uploads/files/solinscripcion ]; then
+    mkdir -p uploads/files/solinscripcion;
+fi 
+if [ ! -d uploads/files/trash ]; then
+    mkdir -p uploads/files/trash;
+fi 
