49ab85247521e5d8b1ce38bff45111d8f4036b22
Danmer Ccoscco <danmerccoscco@gmail.com>
2019-12-04 15:33:07 -0500

    consultas de modelo y relaciones

--- application/models/Discount_model.php
+++ application/models/Discount_model.php
@@ -2,34 +2,72 @@
 
 
 
-class Discount_model extends CI_Model
+class Discount_model extends MY_Model
 {
+	private $table='discounts';
+	
+	private $id='id';
+	private $description='description';
+	private $name='name';
+	private $percentage='percentage';
+
+
+	private $public_columns=['id','description','name','percentage'];
 	
 	function __construct()
 	{
 		parent::__construct();
 	}
 
-	public function registrar($nombre,$porcentaje,$monto,$idTipoBeneficio,$idCreador){
+	public function registrar($nombre,$description,$percentage){
 		$data=array(
-			'idBeneficio' => NULL,
-			'nombrebeneficio' => "$nombre",
-			'porcentajeBeneficio' => "$porcentaje",
-			'montoBeneficio' => "$monto",
-			'idTipoBeneficio' => "$idTipoBeneficio",
-			'creador'=>"$idCreador"
+			$this->name => $nombre,
+			$this->description => $description,
+			$this->percentage => $percentage
 		);
-		$this->db->insert('beneficio',$data);
+		$this->db->insert($this->table,$data);
 		$ultimoId = $this->db->insert_id();
 		return $ultimoId;
 	}
+/*
+	function byCurso($id){
+		$this->db->select($this->table.'.*');
+		$this->db->from($this->table);
+		$this->db->join(
+			'cursos_discount',
+			'cursos_discount.discount_id='.$this->table.'.'.$this->id);
+		$this->db->where('cursos_discount.curso_id',$id);
+		return $this->db->get()->result_array();
+	}
 
+	function bySolicitud($id){
+		$this->db->select($this->table.'.*');
+		$this->db->from($this->table);
+		$this->db->join(
+			'solicitud_discount',
+			'solicitud_discount.discount_id='.$this->table.'.'.$this->id);
+		$this->db->where('solicitud_discount.solicitud_id',$id);
+		return $this->db->get()->result_array();
+	}
+*/
+	protected function relations(){
+		return [
+			'curso'=>[
+				'pivot_table'=>'curso_discounts',
+				'column_relation'=>'discount_id'
+			],
+			'solicitud'=>[
+				'pivot_table'=>'solicitud_discount',
+				'column_relation'=>'discount_id'
+			],
+		];
+	}
 
-	public function all(){
-		$this->db->select('b.idBeneficio,b.nombrebeneficio,b.porcentajeBeneficio,b.montoBeneficio,t.idTipoBeneficio,t.descripcion as nombreTipoBeneficio');    
-		$this->db->from('beneficio b');
-		$this->db->join('tipobeneficio t', 'b.idTipoBeneficio = t.idTipoBeneficio');
-		$query = $this->db->get();
-		return $query;
+	function byCurso($id){
+		return $this->byPivot('curso','curso_id',$id);
+	}
+
+	function bySolicitud($id){
+		return $this->byPivot('solicitud','solicitud_id',$id);
 	}
 }
