125c7bc51c5677321356565a6aa9617b3797ee17
Danmer Ccoscco <danmerccoscco@gmail.com>
2020-07-07 15:54:58 -0500

    get en result

--- application/models/Inscripcion_model.php
+++ application/models/Inscripcion_model.php
@@ -689,7 +689,7 @@ class Inscripcion_model extends CI_Model
 
 	public function resumen(){
 
-		$query = $this->db->select('COUNT(ins.id_inscripcion) as incripcion_count,c.id_curso,c.nombre as nombre_programa,sf.id,sf.nombre as nombre_estado');
+		 $this->db->select('COUNT(ins.id_inscripcion) as incripcion_count,c.id_curso,c.nombre as nombre_programa,sf.id,sf.nombre as nombre_estado');
 		$this->db->from($this->table.' ins');
 		$this->basic_query('ins');
 		$this->db->join('solicitud s','ins.solicitud_id = s.idSolicitud','left');
@@ -699,7 +699,7 @@ class Inscripcion_model extends CI_Model
 		$this->db->join('alumno a','s.alumno = a.id_alumno','left');
 		$this->db->join('estado_finanzas sf','sf.id = ins.id_inscripcion','left');
 		$this->db->group_by(['c.id_curso','ins.estado_finanzas_id']);
-		$this->db->order_by('c.id_curso',"DESC")->get();
+		$query = $this->db->order_by('c.id_curso',"DESC")->get();
 
 		return $query->result();
 	}
