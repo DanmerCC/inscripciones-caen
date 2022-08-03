1c9d23c049b8ffcd31fd8c9a6a3d8d1077beb7ee
Danmer Ccoscco <danmerccosccogmail.com>
2022-08-03 17:06:19 -0500

    endpint

--- application/models/Programa_model.php
+++ application/models/Programa_model.php
@@ -58,7 +58,7 @@ class Programa_model extends CI_Model
 						's.sent_to_inscripcion IS NULL '=>NULL,
 					))
 				->group_end()
-				->order_by('c.s.fecha_registro','DESC')
+				->order_by('s.fecha_registro','DESC')
 				->group_by('c.id_curso');
 		return $result->get();
 	}
