04326e99b1c73cc5943ed7699b544795070f590b
Danmer Ccoscco <danmerccosccogmail.com>
2022-08-09 16:48:20 -0500

    identificado cambiado

--- application/models/Programa_model.php
+++ application/models/Programa_model.php
@@ -123,7 +123,7 @@ class Programa_model extends MY_Model
 						)
 					)
 				->group_end()
-				->order_by('ins.fecha_inicio','DESC')
+				->order_by('c.fecha_inicio','DESC')
 				->group_by('c.id_curso');
 		return $result->get();
 	}
