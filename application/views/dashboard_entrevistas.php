b95d5856e9203985e05af5b75af4b0f533667780
luisfruiz19 <50271618+luisfruiz19@users.noreply.github.com>
2022-11-10 16:43:10 -0500

    mensaje de proxoma caducidad

--- application/views/dashboard_entrevistas.php
+++ application/views/dashboard_entrevistas.php
@@ -50,6 +50,10 @@
 		<!-- Content Wrapper. Contains page content -->
 		<div class="content-wrapper">
 			<!-- Content Header (Page header) -->
+			<div style="z-index: 99999;font-weight: bold; width: 100%; height: 48px; background-color: #e5e53a; display:grid; place-items: center">
+				Esta versión Pronto se dejará de usar, te recomendamos el nuevo sistema de Matriculas
+				<a href="https://matriculas.caen.edu.pe/" target="_blank">enlace aquí</a>
+			</div>
 			<section class="content-header">
 				<h1>
 
